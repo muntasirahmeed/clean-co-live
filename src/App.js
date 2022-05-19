5bd87b6d2bc54b9837666c367d588a33dc653c32
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-05-19 15:06:49 +0600

    login page design done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navber from "./components/Navber";
 import { publicRoute } from "../src/routes/publicRoute";
+import { protectedRoute } from "../src/routes/protectedRoute";
 import { Route, Routes } from "react-router-dom";
 import AOS from "aos";
-import "aos/dist/aos.css"; 
+import "aos/dist/aos.css";
 import { useEffect } from "react";
+import RequireAuth from "./Authentication/RequireAuth";
+import Services from "./pages/Services";
 
 function App() {
   useEffect(() => {
@@ -17,6 +20,9 @@ function App() {
           {publicRoute.map(({ path, Component }, index) => (
             <Route key={index} path={path} element={<Component />} />
           ))}
+          <Route element={<RequireAuth />}>
+            <Route path="/services" element={<Services></Services>}></Route>
+          </Route>
         </Routes>
       </Navber>
     </>
---
+++ src/Authentication/RequireAuth.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const RequireAuth = () => {
+  const user = false;
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return <Outlet/>;
+};
+
+export default RequireAuth;
